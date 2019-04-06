268399b277b087fba9add014f1313988325302a9
JankinJiang <1779827610@qq.com>
2019-04-06 18:04:20 +0800

    add a cpp file named Find

---
+++ Find.cpp
@@ -0,0 +1,59 @@
+#include <algorithm>
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+//返回该元素在数组中的位置，找不到返回-1
+int LineFindval(int num[], int len, int v){
+    for (int i = 0; i < len; i++){
+        if(num[i] == v){
+            return i + 1;
+        }
+    }
+    return -1;
+}
+
+//返回数组中第i个位置的值，i越界返回-1
+int Findp(int num[], int len, int p){
+    if (p <= len && p > 0){
+        return num[p - 1];
+    }
+    return -1;
+}
+
+int BinaryFindval(int num[], int left, int right, int v){
+    int mid = (left + right) / 2;
+    int key = num[mid];
+    if(key == v){
+        return mid + 1;
+    }
+    if(left == right){
+        return -1;
+    }
+    else if(key > v){
+        return BinaryFindval(num, left, mid - 1, v);
+    }
+    else if(key < v){
+        return BinaryFindval(num, mid + 1, right, v);
+    }
+    return -1; //是否可以取消？
+}
+
+
+int main(int argc, char const *argv[])
+{
+    int num[15];
+    for (int i = 0; i < 15; i++){
+        num[i] = rand()%100;
+    }
+    for (int i = 0; i < 15; i++){
+        cout << num[i] << "\t";
+    }
+    cout << endl;
+
+    sort(num, num + 15);
+    cout << BinaryFindval(num, 0, 14, 115) << endl;
+
+    system("pause");
+    return 0;
+}
